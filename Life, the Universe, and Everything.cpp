c91c7e0d8cdaa7a8ce57c61358e99599ac312c01
Nayana Jayanthi <54923867+Nayana-14@users.noreply.github.com>
2021-02-02 12:35:29 +0530

    Life, the Universe, and Everything

--- Life, the Universe, and Everything.cpp
+++ Life, the Universe, and Everything.cpp
@@ -1,23 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int a,b,flag = 0;
-    cin >> a >> b;
-    for(int i = a;i<=b;i++){
-        if(i!=0 && i!=1){
-        for(int j = 2;j<=i/2;j++){
-            if(i%j == 0) {
-                flag = 1;
-                break;
-            }
-            else flag = 0;
-        }
-        if(flag == 0){
-            cout << i << endl;
-        }
-    }
-    }
-    
-    
-    return 0;
+
+int main() {
+	int a;
+	// your code here
+	while(1){
+		cin >> a;
+		if (a == 42) break;
+		cout << a << "\n";
+	}
+
+	return 0;
 }
